c249209bed50653a0d3021414319bc580e4858f1
zql332 <96402264+zql332@users.noreply.github.com>
2021-12-21 14:19:03 +0800

    Update Test.java

--- demo01/cn.ty/test/Test.java
+++ demo01/cn.ty/test/Test.java
@@ -3,5 +3,7 @@ package test;
 public class Test {
     public static void main(String[] args) {
         System.out.println(1111);
+        System.out.println(2222);
+        System.out.println(3333);
     }
 }
